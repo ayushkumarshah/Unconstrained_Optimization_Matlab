39a4e50182c859496035bbd682204027ce645dd1
Ayush Kumar Shah <ayush.kumar.shah@gmail.com>
2020-10-22 18:20:11 +0545

    feat: Correct gradient and Hessians of fun3

--- project_codes/H_fun3.m
+++ project_codes/H_fun3.m
@@ -1,4 +1,5 @@
 function H = H_fun3(x)
 % hessian of poloynomial function
 
-H = [402+400.*x(2,:) 400.*x(1,:); -400.*x(1,:) 200]; % 2x2 metrix
+H = [-400*x(2,:)+1200*x(1,:).^2+2,  -400*x(1,:); 
+     -400*x(1,:), 200];
